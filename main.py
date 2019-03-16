5fb667f6ca10227ef500f4da6122a78f57dffc9b
exoron <twexoron@gmail.com>
2019-03-16 18:47:12 +0300

    Добавил счёт

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 
-
 pygame.init()
+pygame.font.init()
+font_consolas = pygame.font.SysFont('Consolas', 40)
+
 win = pygame.display.set_mode((1000, 1000))
 
 pygame.display.set_caption("Rofl clicker")
@@ -12,8 +14,13 @@ sprite = pygame.image.load('sprites/Nastya.svg.png')
 def draw_window():
     win.fill((128, 0, 255))
     win.blit(sprite, (100, 100))
+    score_text = font_consolas.render('Score: {}'.format(score), False, (0, 0,
+                                                                         0))
+    win.blit(score_text, (200, 50))
     pygame.display.update()
 
+
+score = 0
 
 run = True
 while run:
