f2b60f7783a3c874e2ca5270173730302c620d0a
exoron <twexoron@gmail.com>
2019-03-16 20:33:30 +0300

    Вынес Настю в отдельный класс

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ win = pygame.display.set_mode((1000, 1000))
 
 pygame.display.set_caption("Rofl clicker")
 
-sprite = pygame.image.load('sprites/Nastya.svg.png')
+
+class Nastya(object):
+    def __init__(self):
+        self.sprite = pygame.image.load('sprites/Nastya.svg.png')
+        self.x = 100
+        self.y = 100
+        self.width = 300
+        self.height = 431
+
+    def draw(self, surface):
+        surface.blit(self.sprite, (self.x, self.y))
+
+
+nastya = Nastya()
 
 
 def draw_window():
     win.fill((128, 0, 255))
-    win.blit(sprite, (100, 100))
+    nastya.draw(win)
     score_text = font_consolas.render('Score: {}'.format(score), False, (0, 0,
                                                                          0))
     win.blit(score_text, (200, 50))
